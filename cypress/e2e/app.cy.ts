15089a76ec6546ddf2817011d6ef12cef97845c2
Joice Kelly <114251547+Joice-Kelly@users.noreply.github.com>
2024-08-02 19:23:41 -0400

    Update app.cy.ts
    First commit

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,11 +1,13 @@
 describe('e2e tests', () => {
   it('should display the correct color when parameters are adjusted', () => {
     cy.visit('/');
-    cy.get("[data-cy=red-input]").invoke("val", 255).trigger("change");
-    cy.get("[data-cy=green-input]").invoke("val", 48).trigger("change");
-    cy.get("[data-cy=blue-input]").invoke("val", 139).trigger("change");
-    cy.get("[data-cy=alpha-input]").invoke("val", 1).trigger("change");
+    cy.get('#sum_slider').as("range");
+    cy.get("@range").should("have.length", 1);
+    cy.get("@range").invoke("val", 255).trigger("input");
+    cy.get("@range").invoke("val", 48).trigger("input");
+    cy.get("@range").invoke("val", 139).trigger("input");
+    cy.get("@range").invoke("val", 1).trigger("change");
 
-    cy.get("[data-cy=panel]").should("have.css", "background-color", "rgba(255, 48, 139, 1)");
+    cy.get("@range").should("have.value", "1");
   });
 });
